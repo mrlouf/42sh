a2379bc4192deac44c46193ed9a4fc80de00e287
nponchon <nponchon@student.42.fr>
2025-11-19 14:00:27 +0100

    Trying to see why the script hangs

--- srcs/main/main.c
+++ srcs/main/main.c
@@ -5,6 +5,12 @@ int	main(int ac, char **av, char **env)
 {
 	t_shell	sh;
 
+	if (!isatty(STDIN_FILENO)) {
+		printf("42sh$ ");
+		fflush(stdout);
+		return (0);
+	}
+
 	(void)ac;
 	(void)av;
 	init_shell(&sh, env);
