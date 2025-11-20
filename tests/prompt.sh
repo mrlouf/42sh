5ce0b6fd40981ed7488beb7a6cc3345c8e728df9
Nicolas <ponchon.nicolas@gmail.com>
2025-11-20 17:36:11 +0100

    Adding a bit of formatting

--- tests/prompt.sh
+++ tests/prompt.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 #############################################################################
-# This script tests the prompt generation functionality of the application.	#
-# It verifies that the generated prompts match the expected output for		#
-# various input scenarios.													#
+# This script tests the basic functionalities of the almighty MAXISHELL.	#
+#                                                                           #                                           
 #############################################################################
 
 BASH='/bin/bash'
 MAXISHELL='./42sh'
 
+TOTAL_TESTS=0
+PASSED_TESTS=0
+
+# Test 1: Launch the shell and exit immediately
+TOTAL_TESTS=$((TOTAL_TESTS + 1))
+echo "Test $TOTAL_TESTS: Launch and exit"
 exit | $MAXISHELL
+if [ $? -ne 0 ]; then
+    echo "Test $TOTAL_TESTS Failed: Shell did not exit cleanly."
+else
+    echo "Test $TOTAL_TESTS Passed."
+    PASSED_TESTS=$((PASSED_TESTS + 1))
+fi
