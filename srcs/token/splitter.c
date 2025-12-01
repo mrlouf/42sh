94052112c35b785394073fb7eb4013320644d301
Nicolas <ponchon.nicolas@gmail.com>
2025-12-01 18:07:55 +0100

    Very basic quoted sentence are okay, more complex sentences are causing issues. Created a file with some example to test out and compare against bash.

--- srcs/token/splitter.c
+++ srcs/token/splitter.c
@@ -2,15 +2,21 @@
 
 int	count_words(const char *str, const char *charset)
 {
-	int	i = 0;
-	int	in_word = 0;
-	int	word_count = 0;
+	int		i = 0;
+	int		in_word = 0;
+	int		word_count = 0;
+	char	quote_open = 0;
 
 	while (str[i])
 	{
-		if (ft_strchr(charset, str[i]) != NULL)
+		if (str[i] == '\'' && !quote_open)
+			quote_open = !quote_open;
+		else if (str[i] == '"' && !quote_open)
+			quote_open = !quote_open;
+		else if (ft_strchr(charset, str[i]) && !quote_open) 
 			in_word = 0;
-		else if (in_word == 0)
+
+		if (in_word == 0)
 		{
 			in_word = 1;
 			word_count++;
@@ -37,48 +43,43 @@ static void	free_tokens(char **tokens, int count)
 
 static void	get_words(const char *str, const char *charset, char **tokens)
 {
-	int		i = 0;
-	int		j = 0;
-	int		start = 0;
-	int		in_word = 0;
+    int		i = 0, j = 0, start = -1;
+    int		in_single = 0, in_double = 0;
 
-	while (str[i])
-	{
+    while (str[i])
+    {
+        if (str[i] == '\'' && !in_double)
+            in_single = !in_single;
+        else if (str[i] == '"' && !in_single)
+            in_double = !in_double;
 
-		if (ft_strchr(charset, str[i]) != NULL)
-		{
-			if (in_word)
-			{
-				tokens[j] = ft_substr(str, start, i - start);
-				if (tokens[j] == NULL) {
-					free_tokens(tokens, j);
-					return;
-				}
-				j++;
-				in_word = 0;
-			}
-		}
-		else if (in_word == 0)
-		{
-			in_word = 1;
-			start = i;
-			if (str[i] == '\'' || str[i] == '\"')
-			{
-				char quote = str[i];
-				i++;
-				while (str[i] && str[i] != quote)
-					i++;
-			}
-		}
-		i++;
-	}
-	if (in_word) {
-		tokens[j] = ft_substr(str, start, i - start);
-		if (tokens[j] == NULL) {
-			free_tokens(tokens, j);
-			return;
-		}
-	}
+        if (!in_single && !in_double && ft_strchr(charset, str[i]))
+        {
+            if (start != -1)
+            {
+                tokens[j] = ft_substr(str, start, i - start);
+                if (tokens[j] == NULL) {
+                    free_tokens(tokens, j);
+                    return;
+                }
+                j++;
+                start = -1;
+            }
+        }
+        else
+        {
+            if (start == -1)
+                start = i;
+        }
+        i++;
+    }
+    if (start != -1) {
+        tokens[j] = ft_substr(str, start, i - start);
+        if (tokens[j] == NULL) {
+            free_tokens(tokens, j);
+            return;
+        }
+    }
 }
 
 char **split_charset(const char *str, const char *charset)
--- srcs/token/token.c
+++ srcs/token/token.c
@@ -1,7 +1,7 @@
 #include "../incs/42sh.h"
 #include "../incs/token.h"
 
-void		tokenise_input(t_shell *sh, char *input)
+void	tokenise_input(t_shell *sh, char *input)
 {
 	char **tokens = NULL;
 
@@ -13,9 +13,16 @@ void		tokenise_input(t_shell *sh, char *input)
 	write_history(0);
 
 	tokens = split_charset(input, sh->ifs);
+	if (!tokens)
+	{
+		ft_putstr_fd("Error: Malloc: Tokenisation failed\n", 2);
+		free(input);
+		exit(1);
+	}
 
+	printf("%zu tokens:\n", ft_array_size((void **)tokens));
 	for (int i = 0; tokens[i] != NULL; i++)
-	{ printf("\"%s\" ", tokens[i]); }
+	{ printf("^%s^ ", tokens[i]); }
 	printf("\n");
 	ft_array_free((void **)tokens);
 }
