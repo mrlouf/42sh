7a4f1d8c3b03078316df80eccb7a08af1b171829
Hugo Munoz Gris <hmunoz-g@car13s2.42barcelona.com>
2025-12-03 11:03:41 +0100

    some fixes

--- incs/env.h
+++ incs/env.h
@@ -7,6 +7,7 @@ unsigned int	hash_string(const char *str, unsigned int table_size);
 t_var_table		*init_var_table(void);
 int				set_variable(t_var_table *table, const char *name, const char *value, int exported, int was_equalized);
 int				unset_variable(t_var_table *table, const char *name);
+int				variable_exists(t_var_table *table, const char *name);
 char			*get_variable(t_var_table *table, const char *name);
 int				count_stored_env_variables(t_var_table *table);
 int				compare_vars(const void *a, const void *b);
@@ -15,7 +16,7 @@ int				mark_variable_as_readonly(t_var_table *table, const char *name);
 int				is_variable_exported(t_var_table *table, const char *name);
 int				is_variable_readonly(t_var_table *table, const char *name);
 t_var			**get_all_sorted_refs(t_var_table *table, size_t *count);
-t_var			**get_sorted_variable_refs(t_var_table *table, size_t *count);
+t_var			**get_sorted_exported_refs(t_var_table *table, size_t *count);
 t_var			**get_sorted_readonly_refs(t_var_table *table, size_t *count);
 void			free_var_table(t_var_table *table);
 
